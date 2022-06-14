2e6d820b82836948a33f9c9abbd710f17f33ea8d
AudhyBrilliantPratama <audhybrilliant@gmail.com>
2022-06-14 17:19:05 +0700

    chore:sync

---
+++ Frontend/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import LoginUi from './page/Login';
+function App() {
+  return (
+    <LoginUi />
+  );
+}
+
+export default App;
---
+++ Frontend/src/page/Login.jsx
@@ -0,0 +1,17 @@
+// import familyBg from "../asset/img/Bg/family-bg.jpg";
+
+import LoginFormComponent from "../components/LoginForm";
+
+import '../asset/css/Login.css';
+
+function LoginUi () {
+    return (
+        <div className="container">
+            <h1 className="title-1">Pes<span>Buk</span></h1>
+            <LoginFormComponent />
+        </div>
+    );
+    
+}
+
+export default LoginUi;
