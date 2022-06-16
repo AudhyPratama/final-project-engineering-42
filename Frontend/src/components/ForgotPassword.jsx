0474145061215a2cd93ac7d884ebf1dfd36c8c12
AudhyBrilliantPratama <audhybrilliant@gmail.com>
2022-06-16 12:12:04 +0700

    added 2 new component and more styling in login page

---
+++ Frontend/src/components/ForgotPassword.jsx
@@ -0,0 +1,11 @@
+import LoginFormComponent from "./LoginForm";
+
+function ForgotPassword () {
+    return (
+        <div className='forgot-password-form'>
+            <h1>ini forgot password</h1>
+        </div>
+    );
+}
+
+export default ForgotPassword;
